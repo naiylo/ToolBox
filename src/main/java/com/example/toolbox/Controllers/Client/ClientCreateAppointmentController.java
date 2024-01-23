1ee7de69a488ec4902e970e3ab2cf99ec204cd8b
Oskar Schiedewitz <schiedewitz1@uni-potsdam.de>
2024-01-23 18:45:27 +0100

    -Prepared the View for the Appointment Creation

---
+++ src/main/java/com/example/toolbox/Controllers/Client/ClientCreateAppointmentController.java
@@ -0,0 +1,18 @@
+package com.example.toolbox.Controllers.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCreateAppointmentController implements Initializable {
+    public TextField date_textfield;
+    public TextField timeslot_textfield;
+    public TextField description_textfield;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
